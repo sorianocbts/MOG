ee7900901f61fbd928c4778fcc7e9f2c0e769183
Robert <soriano@cbtseminary.org>
2021-03-23 08:55:15 -0500

    .

--- pages/confession.js
+++ pages/confession.js
@@ -23,21 +23,50 @@ const Confession = () => {
                 text-transform: capitalize;
                 font-family: 'Open Sans' !important;
                 font-weight:400 !important;
-                font-size: 12px !important;
+                font-size: 13px !important;
                 color: blue !important;
+            }
+            .ch-link {
+                color: #272B2B !important;
+            }
+            .ch-link:hover {
+                color: blue !important;
+                text-decoration: underline;
             }
             `}
             </style>
             <div className={`container confession-container shadow`}>
-                <div className={`mb-1`}><h1 className={`text-center`}>The London Baptist Confession of Faith of 1689</h1></div>
-                {LBCF["Data"].map((chapter, chapterIndex) => (
-                    <>
-                        <div className={`chapter-container `}>
-                            <Link href={`/confession/#${formatURI(chapter["Title"])}`}><a onClick={() => goToAnchor(`${formatURI(chapter["Title"])}`)}>
-                                <h4 className={`text-left font-light`}>Chapter {chapter["Chapter"]}: {chapter["Title"]}</h4></a></Link>
-                        </div>
-                    </>
-                ))}
+                <div className={`mb-4`}><h1 className={`text-center`}>The London Baptist Confession of Faith of 1689</h1></div>
+                <div className={`row flex-wrap`}>
+                    <div className={`col-sm-12 col-md-4`}>
+                        {LBCF["Data"].slice(0, 11).map((chapter, chapterIndex) => (
+                            <>
+                                <div className={`chapter-container `} key={chapterIndex}>
+                                    <Link href={`/confession/#${formatURI(chapter["Title"])}`}><a onClick={() => goToAnchor(`${formatURI(chapter["Title"])}`)}>
+                                        <h4 className={`text-left font-light ch-link`}><strong>Chapter {chapter["Chapter"]}</strong>: {chapter["Title"]}</h4></a></Link>
+                                </div>
+                            </>
+                        ))}</div>
+                    <div className={`col-sm-12 col-md-4`}>
+                        {LBCF["Data"].slice(11, 22).map((chapter, chapterIndex) => (
+                            <>
+                                <div className={`chapter-container `} key={chapterIndex}>
+                                    <Link href={`/confession/#${formatURI(chapter["Title"])}`}><a onClick={() => goToAnchor(`${formatURI(chapter["Title"])}`)}>
+                                        <h4 className={`text-left font-light ch-link`}><strong>Chapter {chapter["Chapter"]}</strong>: {chapter["Title"]}</h4></a></Link>
+                                </div>
+                            </>
+                        ))}</div>
+                    <div className={`col-sm-12 col-md-4`}>
+                        {LBCF["Data"].slice(22, 32).map((chapter, chapterIndex) => (
+                            <>
+                                <div className={`chapter-container `} key={chapterIndex}>
+                                    <Link href={`/confession/#${formatURI(chapter["Title"])}`}><a onClick={() => goToAnchor(`${formatURI(chapter["Title"])}`)}>
+                                        <h4 className={`text-left font-light ch-link`}><strong>Chapter {chapter["Chapter"]}</strong>: {chapter["Title"]}</h4></a></Link>
+                                </div>
+                            </>
+                        ))}</div>
+
+                </div>
                 {LBCF["Data"].map((chapter, chapterIndex) => (
                     <>
                         <ScrollableAnchor id={`${formatURI(chapter["Title"])}`} key={chapterIndex}>
